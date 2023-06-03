ca9d603a379e7ac4cb8dd55597def47f2f93d243
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-06-03 13:07:53 +0000

    [Add]ActiveStrage追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,15 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items
   has_many :order_items
+
+  has_one_attached :image
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [100, 100]).processed
+  end
+
 end
