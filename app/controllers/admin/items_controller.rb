7d4bf058add32fc3c69961b1316abb87b7b66201
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-06-05 13:47:08 +0000

    [Add]管理者側,itemsコントローラー記述追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,31 +1,47 @@
 class Admin::ItemsController < ApplicationController
 
   def new
-
+    @item = Item.new
   end
 
   def index
-
+    @items = Item.all
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "商品の保存に成功しました。"
+      redirect_to item_path(@item.id)
+    else
+      @item = Item.all
+      render :new
+    end
   end
 
   def show
-
+    @item = Item.find(params[:id])
   end
 
   def edit
-
+    @item = Item.find(params[:id])
   end
 
   def update
-
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "編集に成功しました。"
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    item = Item.find(params[:id])
+    item.destroy
+    flash[:notice] = "商品を削除しました。"
+    redirect_to items_path
   end
 
 private
