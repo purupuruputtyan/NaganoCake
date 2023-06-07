ef2505cac98baa40b1538df88567054db5986375
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-06-07 13:20:33 +0000

    [Add]管理者側customersコントローラー記述追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,41 @@
 class Admin::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "編集が成功しました。"
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :first_name,
+      :family_name_kana,
+      :first_name_kana,
+      :post_code,
+      :address,
+      :phone_number,
+      :email,
+      :is_deleted
+    )
+  end
+
 end
--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,11 @@
 class Admin::OrderItemsController < ApplicationController
+  
+  def show
+    
+  end
+  
+  def update
+  
+  end
+  
 end
