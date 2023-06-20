51b172c7818eb3e086c9fe9fea61d80b1d3c582e
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-06-20 11:46:00 +0000

    [Add]会員側、新規登録画面追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,23 @@
 class Public::CustomersController < ApplicationController
+  
+  def index
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :phone_number)
+  end
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -38,12 +38,21 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer
+      .permit(:sign_up,
+        keys: [:family_name,
+               :first_name,
+               :family_name_kana,
+               :first_name_kana,
+               :post_code,
+               :address,
+               :phone_number
+      ])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -51,9 +60,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(resource)
   #   super(resource)
-  # end
+    root_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
