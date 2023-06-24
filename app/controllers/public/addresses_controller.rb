0beb0f530e4df8a8840fdf251d621b6b20f2f3fd
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-06-24 14:22:13 +0000

    [Add]会員側,addressesコントローラー,記述追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,44 @@
 class Public::AddressesController < ApplicationController
+
+  def index
+    @addesses = Address.all
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      flash[:notice] = "新しい住所を保存しました。"
+      redirect_to addresses_path
+    else
+      @addesses = Address.all
+      render :index
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] = "編集に成功しました。"
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    flash[:notice] = "住所を削除しました。"
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :post_code, :address)
+  end
+
 end
