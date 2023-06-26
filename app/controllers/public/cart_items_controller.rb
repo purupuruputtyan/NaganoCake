6879cd0e75aac4eff1e2c2219ee0b2c3bfbdf58c
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-06-26 14:27:10 +0000

    [Add]会員側、public/items/view、記述追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,46 @@
 class Public::CartItemsController < ApplicationController
+
+  def index
+    @cart_items = current_customer.cart_items
+  end
+
+  def create
+    if @cart_item
+      new_amount = @cart_item.amount + cart_item_params[:amount]
+      @cart_item.update(amount: new_amount)
+      redirect_to cart_items_path
+    else
+      @cart_item = current_customer.cart_items.new(cart_item_params)
+      @cart_item.item_id = @item.id
+      if @cart_item.save
+        flash[:notice] = "カートに入りました。"
+        redirect_to cart_items_path
+      else
+        render 'public/items/show'
+      end
+    end
+  end
+
+  def index
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def destroy_all
+
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
+  end
+
 end
