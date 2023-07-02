928ad5fa18f91da7006b1cc3cd95eafff5c0ced7
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-07-02 14:38:24 +0000

    [Add]会員側、orders/views/new、記述追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,6 @@ class Order < ApplicationRecord
   validates :post_code, presence: true, length: {maximum:7}
   validates :address, presence: true
   validates :name, presence: true
+  
+  enum payment_method: { credit_card: 0, transfer: 1 }
 end
